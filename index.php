d842a49a7777f239b192911b4b2a12acd27e39c7
rolanr83 <60845792+rolanr83@users.noreply.github.com>
2022-10-02 21:14:46 -0500

    add two additional button

--- index.php
+++ index.php
@@ -27,5 +27,7 @@
     ?>
 
 <button type="button" class="btn btn-dark">CLICK ME</button>
+<button type="button" class="btn btn-primary">CLICK ME</button>
+<button type="button" class="btn btn-success">CLICK ME</button>
 
 <?php require 'includes/footer.php' ?>
